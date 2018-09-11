f44bda276d5ed56fc6fd6b96555b18005b47976e
Christopher Auyeung <chris@winfieldco.com>
2018-09-11 12:47:07 -0400

    replace deprecated SortedDict

--- tinymce/widgets.py
+++ tinymce/widgets.py
@@ -14,7 +14,7 @@ from django.contrib.admin import widgets as admin_widgets
 from django.core.urlresolvers import reverse
 from django.forms.widgets import flatatt
 from django.utils.html import escape
-from django.utils.datastructures import SortedDict
+from collections import OrderedDict as SortedDict
 from django.utils.safestring import mark_safe
 from django.utils.translation import get_language, ugettext as _
 try:
